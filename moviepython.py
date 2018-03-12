3077233fa20faa5cdfa6c0385f6bd68bfd807998
Tre vonne Maurice Robinson <trevonne.robinson@stu.jefferson.kyschools.us>
2018-03-12 08:34:36 -0400

    first commit

---
+++ moviepython.py
@@ -0,0 +1,9 @@
+# Trevonne M. Robinson 3/1/2018
+
+
+media_type = input("What is the media type")
+title = input ("What is the title")
+descrip = input (" give me a brief desription")
+year = (str( input(" What year did the movie come out")
+rating = (float(input (" What type of rating do you give this movie 1/10")
+
